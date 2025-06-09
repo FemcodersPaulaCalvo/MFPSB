41907fdc9210a4e8ae1d8c1e051c8d365c9bc695
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-09 18:28:28 +0200

    feat: entity Author --> Id, name + getters/setters + constructor

--- src/main/java/com/MyFirstProjectSpringBoot/MFPSB/entity/Author.java
+++ src/main/java/com/MyFirstProjectSpringBoot/MFPSB/entity/Author.java
@@ -1,4 +1,36 @@
 package com.MyFirstProjectSpringBoot.MFPSB.entity;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @OneToMany
+    @JoinColumn
+    private String name;
+
+    public Author() {
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
