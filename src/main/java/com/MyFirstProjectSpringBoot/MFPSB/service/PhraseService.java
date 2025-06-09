fe9b40ad49d91282104160685c1da88ceab84d3a
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-09 20:39:21 +0200

    feat: service PhraseService --> add repository PhraseRepository

---
+++ src/main/java/com/MyFirstProjectSpringBoot/MFPSB/service/PhraseService.java
@@ -0,0 +1,13 @@
+package com.MyFirstProjectSpringBoot.MFPSB.service;
+
+import com.MyFirstProjectSpringBoot.MFPSB.repository.PhraseRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhraseService {
+
+    private final PhraseRepository PHRASE_REPOSITORY;
+
+
+
+}
