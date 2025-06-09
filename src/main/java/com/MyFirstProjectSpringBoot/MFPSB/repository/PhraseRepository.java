88e40ee62cc357ad6aa6c5a5e1bdd6c3956e4e39
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-09 19:11:45 +0200

    feat: repository PhraseRepository methods --> findAll, save,  deleteById, findById, getByAuthor, getÃByCategory

--- src/main/java/com/MyFirstProjectSpringBoot/MFPSB/repository/PhraseRepository.java
+++ src/main/java/com/MyFirstProjectSpringBoot/MFPSB/repository/PhraseRepository.java
@@ -1,4 +1,24 @@
 package com.MyFirstProjectSpringBoot.MFPSB.repository;
 
-public class PhraseRepository {
+import com.MyFirstProjectSpringBoot.MFPSB.entity.Author;
+import com.MyFirstProjectSpringBoot.MFPSB.entity.Category;
+import com.MyFirstProjectSpringBoot.MFPSB.entity.Phrase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PhraseRepository extends JpaRepository<Phrase, Long> {
+    List<Phrase> findAll();
+
+    Phrase save(Phrase phrase);
+
+    void deleteById(Long id);
+
+    Optional<Phrase> findById(Long id);
+
+    List<Phrase> getByAuthor(Author author);
+
+    List<Phrase> getByCategory(Category category);
+
 }
