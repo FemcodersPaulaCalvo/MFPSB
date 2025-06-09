1cb43024c8f3fe0321a8db1bfa27dc9c10d8af98
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-09 18:25:39 +0200

    feat: entity Phrase --> Id, text, author, category + getters/setters + constructor

--- src/main/java/com/MyFirstProjectSpringBoot/MFPSB/entity/Phrase.java
+++ src/main/java/com/MyFirstProjectSpringBoot/MFPSB/entity/Phrase.java
@@ -1,4 +1,60 @@
 package com.MyFirstProjectSpringBoot.MFPSB.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Phrases")
 public class Phrase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name="author_id")
+    private Author author;
+
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+
+    public Phrase() {
+    }
+
+    public Phrase(String text, Author author, Category category) {
+        this.text = text;
+        this.author = author;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
