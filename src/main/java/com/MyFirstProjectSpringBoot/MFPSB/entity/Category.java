12538ab48ed2afbbbc0f494e11653e6dab75f8da
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-09 18:36:11 +0200

    feat: entity Category --> Id, name + getters/setters + constructor

--- src/main/java/com/MyFirstProjectSpringBoot/MFPSB/entity/Category.java
+++ src/main/java/com/MyFirstProjectSpringBoot/MFPSB/entity/Category.java
@@ -1,4 +1,33 @@
 package com.MyFirstProjectSpringBoot.MFPSB.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
