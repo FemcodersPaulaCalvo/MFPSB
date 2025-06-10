150b94a338b3cc659ce9eab4a1c21a0444ea9bc6
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-10 17:00:16 +0200

    feat: PhraseController --> method postNewPhrase

--- src/main/java/com/MyFirstProjectSpringBoot/MFPSB/controller/PhraseController.java
+++ src/main/java/com/MyFirstProjectSpringBoot/MFPSB/controller/PhraseController.java
@@ -1,24 +1,22 @@
 package com.MyFirstProjectSpringBoot.MFPSB.controller;
 
+import com.MyFirstProjectSpringBoot.MFPSB.dto.RequestPhraseDto;
 import com.MyFirstProjectSpringBoot.MFPSB.dto.ResponsePhraseDto;
 import com.MyFirstProjectSpringBoot.MFPSB.repository.PhraseRepository;
 import com.MyFirstProjectSpringBoot.MFPSB.service.PhraseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/phrases")
 public class PhraseController {
-    private final PhraseRepository PHRASE_REPOSITORY;
+
     private final PhraseService PHRASE_SERVICE;
 
-    public PhraseController(PhraseRepository PHRASE_REPOSITORY, PhraseService PHRASE_SERVICE) {
-        this.PHRASE_REPOSITORY = PHRASE_REPOSITORY;
+    public PhraseController(PhraseService PHRASE_SERVICE) {
         this.PHRASE_SERVICE = PHRASE_SERVICE;
     }
 
@@ -26,5 +24,11 @@ public class PhraseController {
     public ResponseEntity<List<ResponsePhraseDto>> getAllPhrasesList(){
         List<ResponsePhraseDto> phrases = PHRASE_SERVICE.findAllPhrases();
         return new ResponseEntity<>(phrases, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponsePhraseDto> postNewPhrase(@RequestBody RequestPhraseDto requestPhraseDto){
+        ResponsePhraseDto newPhrase = PHRASE_SERVICE.createPhrase(requestPhraseDto);
+        return new ResponseEntity<>(newPhrase, HttpStatus.OK);
     }
 }
