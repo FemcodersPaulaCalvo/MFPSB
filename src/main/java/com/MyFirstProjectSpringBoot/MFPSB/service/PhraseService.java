4da5b38e7ec925d67f85deb6c110017bf92dde4e
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-10 16:48:35 +0200

    feat: PhraseService --> add method createPhrase

--- src/main/java/com/MyFirstProjectSpringBoot/MFPSB/service/PhraseService.java
+++ src/main/java/com/MyFirstProjectSpringBoot/MFPSB/service/PhraseService.java
@@ -1,11 +1,13 @@
 package com.MyFirstProjectSpringBoot.MFPSB.service;
 
+import com.MyFirstProjectSpringBoot.MFPSB.dto.RequestPhraseDto;
 import com.MyFirstProjectSpringBoot.MFPSB.dto.ResponsePhraseDto;
 import com.MyFirstProjectSpringBoot.MFPSB.entity.Phrase;
 import com.MyFirstProjectSpringBoot.MFPSB.repository.PhraseRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PhraseService {
@@ -27,6 +29,16 @@ public class PhraseService {
                 .toList();
     }
 
+    //  POST NEW PHRASE
+    public ResponsePhraseDto createPhrase(RequestPhraseDto requestPhraseDto){
+        Optional<Phrase> isExisting = PHRASE_REPOSITORY.findByText(requestPhraseDto.text());
+        if (isExisting.isPresent()){
+            throw new RuntimeException("This phrase exists: " + isExisting.get().getText() + " Author:" + isExisting.get().getAuthor());
+        }
 
+        Phrase newPhrase = requestPhraseDto.toEntity();
+        Phrase savedPhrase = PHRASE_REPOSITORY.save(newPhrase);
+        return ResponsePhraseDto.fromEntity(savedPhrase);
+    }
 
 }
