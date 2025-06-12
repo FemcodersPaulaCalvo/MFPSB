c82af84a06d4f37aa692adc221b74964f7150a01
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-06-12 09:33:04 +0200

    feat: CategoryRepository --> add findAll

--- src/main/java/com/MyFirstProjectSpringBoot/MFPSB/repository/CategoryRepository.java
+++ src/main/java/com/MyFirstProjectSpringBoot/MFPSB/repository/CategoryRepository.java
@@ -3,10 +3,13 @@ package com.MyFirstProjectSpringBoot.MFPSB.repository;
 import com.MyFirstProjectSpringBoot.MFPSB.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category save(Category category);
 
     Optional<Category> findByName(String name);
+
+    List<Category> findAll();
 }
